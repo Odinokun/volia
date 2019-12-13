5c052a00c22b349b89a8e8a22b98e51d80fa7779
odinokun <odinokun@gmail.com>
2019-12-13 14:34:19 +0200

    did top section

--- src/js/app.js
+++ src/js/app.js
@@ -3,30 +3,16 @@ $(function() {
   // begin SVG for IE
   require('./modules/svg4everybody')();
 
-  // begin no-JS
-  require('./modules/noJs')();
-
-  // begin phone mask
-  // require('./modules/maskedinput')();
-
   // begin Scroll2Ancor
-  // require('./modules/scroll2ancor')();
+  require('./modules/scroll2ancor')();
 
   // begin Slick slider
-  // require('./modules/slick')();
+  require('./modules/slick')();
 
   // begin Popup
-  // require('./modules/popup')();
-
-  // begin Sticky header
-  // require('./modules/stickyHeader')();
+  require('./modules/popup')();
 
   // begin Animate.css
-  //РАБОТАЕТ ТОЛЬКО С JQUERY < 3.0.0 !!!!!!!!!!!!!!!!!!
-  // require('./modules/waypoints')();
   // require('./modules/animateCss')();
-
-  // begin Fancybox3
-  // require('./modules/fancybox')();
 
 });
--- src/js/modules/maskedinput.js
+++
@@ -1,9 +0,0 @@
-module.exports = function() {
-
-  // begin maskedinpt
- $(function(){
-   $(".phone-mask").mask("+7(999)999-99-99");
- });
-  // end maskedinpt
-
-};

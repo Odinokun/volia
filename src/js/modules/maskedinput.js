554332d24b4fdbea72591168299e348139cb0cf2
odinokun <odinokun@gmail.com>
2019-12-24 19:01:57 +0200

    change logo in header

--- src/js/modules/maskedinput.js
+++ src/js/modules/maskedinput.js
@@ -1,9 +1,9 @@
 module.exports = function() {
 
   // begin maskedinpt
- $(function(){
-   $(".phone-mask").mask("+7(999)999-99-99");
- });
+   $(function(){
+     $(".phone-mask").mask("+38 (099) 999-99-99");
+   });
   // end maskedinpt
 
 };
